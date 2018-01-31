a1fce2eae9f2044a5f14d759456d8827611dc4ed
John Punch <cakama3a@gmail.com>
2018-01-31 13:16:03 +0200

    RuMiWIFI 1.6.7.2

---
+++ js/popup.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+	chrome.storage.local.get("extensionMode",function (results){
+        var mode = results.extensionMode;
+        switch(mode) {
+                // 0: off; 1: aspect; 2: zoom;
+            case 0:
+                $("#chatOff").addClass("active");
+                break;
+            case 1:
+                $("#chatOn").addClass("active");
+			break;
+        }
+    });
+	$("#chatOff").click(function() {
+        chrome.storage.local.set({"extensionMode":0},function (){
+			$("button").removeClass("active");
+			$("#chatOff").addClass("active");
+        });
+    });
+	$("#chatOn").click(function() {
+        chrome.storage.local.set({"extensionMode":1},function (){
+			$("button").removeClass("active");
+			$("#chatOn").addClass("active");
+        });
+    });
+	
+	chrome.storage.local.get("rumiLang",function (results){
+		var mode = results.rumiLang;
+		if (mode) {
+			$("#lang").val(mode);
+		}
+	});
+	
+	$("#lang").change(function() {
+		val = $(this).val();
+		chrome.storage.local.set({"rumiLang":val});
+	});
+});
