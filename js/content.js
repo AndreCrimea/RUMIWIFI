2c060347272c63a9bb465b8425cc38aacf97adff
AndreCrimea <36231814+AndreCrimea@users.noreply.github.com>
2018-02-09 19:00:11 +0300

    Update content.js
    Исправления и уточнения

--- js/content.js
+++ js/content.js
@@ -50,8 +50,8 @@
 			listOf += "#testBand:contains('开'),";
 			listOf += "p:contains('自'),";
 			listOf += "h4:contains('家'),";
-			listOf += "span:contains('家'),";
-			listOf += "p:contains('家'),";
+			listOf += "span#sysmenu:contains('家'),";
+			listOf += ".router.nav-tab.active p:contains('家'),";
 			listOf += "#qosmode ul li:contains('模'),";
 			listOf += "#qosmode ul li:contains('先'),";
 			listOf += "h4:contains('根'),";
@@ -305,7 +305,7 @@
 			listOf += "#natlist_port tr td:contains('TCP和UDP'),";
 			listOf += "#bandlist tr td:contains('没有设置信息'),";
 			listOf += "#upnplist tr td:contains('没有UPnP设备'),";
-			listOf += ".btn span:contains('限速设置')";
+			listOf += "#btnBandset span:contains('限速设置')";
 		
 			//Базовые замены
 			$(listOf).each(function() {
